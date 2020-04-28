f6ae6ae5684cd644f3f0abb4716961958a866ee4
diego <diegolima362@gmail.com>
2020-04-27 21:46:30 -0300

    todo app

---
+++ todos/index.js
@@ -0,0 +1,86 @@
+const electron = require('electron');
+
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+
+let mainWindow;
+let addWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadURL(`file://${__dirname}/main.html`);
+    mainWindow.on('close', () => {
+        app.quit();
+    });
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
+});
+
+ipcMain.on('todo:add', (event, todo) => {
+    mainWindow.webContents.send('todo:add', todo);
+    addWindow.close();
+});
+
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add New Todo',
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    addWindow.loadURL(`file://${__dirname}/add.html`);
+    addWindow.on('closed', () => { addWindow = null });
+}
+
+const menuTemplate = [{
+    label: 'File',
+    submenu: [{
+            label: 'New Todo',
+            click() {
+                createAddWindow();
+            }
+        },
+        {
+            label: 'Clear Todos',
+            click() {
+                mainWindow.webContents.send('todo:clear');
+            }
+        },
+        {
+            label: 'Quit',
+            accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+            click() {
+                app.quit();
+            }
+        },
+    ]
+}];
+
+if (process.platform === 'darwin') {
+    menuTemplate.unshift({});
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    menuTemplate.push({
+        label: 'View',
+        submenu: [
+            { role: 'reload' },
+            {
+                label: 'Toggle Developer Tools',
+                accelerator: process.platform === 'darwin' ? 'Command+Alt+I' : 'Ctrl+Shift+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            }
+        ]
+    });
+}
