93c2c3e98e501c2d85e70ecd55eea8855ab09390
diego <diegolima362@gmail.com>
2020-04-29 22:20:48 -0300

    start video converter app: load metadata from multiple files, use of promises

---
+++ videoconvert/index.js
@@ -0,0 +1,37 @@
+const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+const _ = require('lodash');
+
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 600,
+        webPreferences: {
+            backgroundThrottling: false,
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+});
+
+ipcMain.on('videos:added', (event, videos) => {
+
+    const promises = _.map(videos, video => {
+        return new Promise((resolve, reject) => {
+            ffmpeg.ffprobe(video.path, (err, metadata) => {
+                resolve(metadata);
+            });
+        });
+    });
+
+    Promise.all(promises)
+        .then((results) => {
+            console.log(results);
+        });
+
+});
